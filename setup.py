3c1bc258b64f2511078263fbde82169f3f63089f
jlazear <jlazear@gmail.com>
2014-10-24 16:46:32 -0400

    Changed from distutils to setuptools.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='PyOscope',
       version='1.0',
       description='Realtime plotter of data from files',
       author='Justin Lazear',
       author_email='jlazear@gmail.com',
-      url='http://www.github.com/jlazear/pyoscope',
+      url='https://www.github.com/jlazear/pyoscope',
       py_modules=['pyoscope', 'readers']
       )
